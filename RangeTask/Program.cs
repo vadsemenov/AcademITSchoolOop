7a3562726f48c8c55068e547b4c15e4007746ac8
Vadim Semenov <semenov@comfortlab.ru>
2023-01-25 17:11:37 +0300

    Add gitIgnore

--- RangeTask/Program.cs
+++ RangeTask/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace RangeTask;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("asdfasdf");
+    }
+}
