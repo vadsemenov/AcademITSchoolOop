602f10d62cde6b56ded2b3830b18c07e7f0ffd7e
Vadim Semenov <semenov@comfortlab.ru>
2023-02-21 14:58:09 +0300

    Add ArrayListHome

---
+++ ArrayListHomeTask/ListUtils.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ArrayListHomeTask
+{
+    public static class ListUtils
+    {
+        public static List<string> GetFileLinesList(this FileInfo file)
+        {
+            var lines = new List<string>();
+
+            if (!file.Exists)
+            {
+                return lines;
+            }
+
+            using var reader = new StreamReader(file.FullName);
+
+            while (reader.ReadLine() is { } line)
+            {
+                lines.Add(line);
+            }
+
+            return lines;
+        }
+
+        public static List<int> RemoveEvenNumbers(this List<int> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] % 2 == 0)
+                {
+                    list.RemoveAt(i);
+                    i--;
+                }
+            }
+
+            return list;
+        }
+
+        public static List<int> GetNotRepeatingNumbersList(this List<int> list)
+        {
+            var resultList = new List<int>();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (!resultList.Contains(list[i]))
+                {
+                    resultList.Add(list[i]);
+                }
+            }
+
+            return resultList;
+            // return list.Distinct().ToList();
+        }
+    }
+}
---
+++ ArrayListHomeTask/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ArrayListHomeTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var textFileLines = new FileInfo("..\\..\\1.txt").GetFileLinesList();
+
+            Console.WriteLine("Содержимое файла:");
+            foreach (var line in textFileLines)
+            {
+                Console.WriteLine(line);
+            }
+
+            var list = new List<int>() { 1, 5, 2, 1, 3, 5 }.RemoveEvenNumbers();
+            Console.WriteLine("Список после удаления четных чисел: " + string.Join(" ", list));
+
+            Console.WriteLine("Список после удаления повторяющихсял чисел: " + string.Join(" ", list.GetNotRepeatingNumbersList()));
+
+            Console.Read();
+        }
+    }
+}
