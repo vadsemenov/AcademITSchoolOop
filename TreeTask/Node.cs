f58060652c0a637229cf3ca7ca2247751f05c50c
Vadim Semenov <semenov@comfortlab.ru>
2023-03-09 22:34:30 +0300

    Add BST

---
+++ TreeTask/Node.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TreeTask
+{
+    public class Node<T> : ICloneable
+    {
+        public T Value { get; set; }
+
+        public Node<T> Left;
+        public Node<T> Right;
+
+        public Node(T value)
+        {
+            Value = value;
+            Left = null;
+            Right = null;
+        }
+
+        public object Clone()
+        {
+            var newNode = (Node<T>)this.MemberwiseClone();
+            newNode.Value = this.Value;
+            newNode.Left = this.Left;
+            newNode.Right = this.Right;
+
+            return newNode;
+        }
+
+        public Node<T> CloneNode()
+        {
+            var newNode = (Node<T>)this.MemberwiseClone();
+            newNode.Value = this.Value;
+            newNode.Left = this.Left;
+            newNode.Right = this.Right;
+
+            return newNode;
+        }
+    }
+}
